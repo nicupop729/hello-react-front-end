02b1d68e7b2a2f3ac3933176f5f76c8fe6f0d2ac
Nicolae Pop <nicupop729@gmail.com>
2022-03-29 20:34:40 -0400

    Create the Greeting component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => (
+  <div>
+    <h2>Random greet for you:</h2>
+    <p>
+      <em>Your greet will be here</em>
+    </p>
+  </div>
+);
+
+export default Greeting;
